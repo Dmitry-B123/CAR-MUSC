2073427292e5f3f50ad3cf70e8b474773bc9605d
Dmitry <vaipens666@gmail.com>
2022-11-01 20:56:43 +0300

    achievements and slider

---
+++ src/assets/js/slider.js
@@ -0,0 +1,9 @@
+$('.slider__content').slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 1000,
+   prevArrow: $('.arrow-left'),
+   nextArrow: $('.arrow-right'),
+});
