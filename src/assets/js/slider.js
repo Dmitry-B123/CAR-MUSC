c789a4820b283f680bcdd87363d2eb0dbd496f5e
Dmitry <vaipens666@gmail.com>
2022-11-02 22:37:52 +0300

    info-1

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,9 +1,19 @@
+//slider1
 $('.slider__content').slick({
    infinite: true,
    slidesToShow: 3,
    slidesToScroll: 1,
    autoplay: true,
    autoplaySpeed: 1000,
-   prevArrow: $('.arrow-left'),
-   nextArrow: $('.arrow-right'),
+   prevArrow: $('.slider__1'),
+   nextArrow: $('.slider__2'),
+});
+
+//slider2
+$('.info__slider-block').slick({
+   infinite: true,
+   slidesToScroll: 1,
+   autoplay: false,
+   prevArrow: $('.info__slider-1'),
+   nextArrow: $('.info__slider-2'),
 });
